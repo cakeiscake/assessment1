96d907142d0e79ed53da5392d10e6e173efea945
cake <liveinfantasy@gmail.com>
2019-03-04 17:09:24 -0500

    working api key insert

--- ttrader/data/seed.py
+++ ttrader/data/seed.py
@@ -2,6 +2,7 @@ import os
 import time
 from app.orm import ORM
 from app import Account, Position, Trade
+from app.util import gen_api_key
 
 DIR = os.path.dirname(__file__)
 DBFILENAME = 'ttrader.db'
@@ -10,14 +11,14 @@ DBPATH = os.path.join(DIR, DBFILENAME)
 def seed(dbpath=DBPATH):
     ORM.dbpath = dbpath
     
-    mike_bloom = Account(username='mike_bloom', balance=10000.00)
+    api_key = gen_api_key()
+    mike_bloom = Account(username='mike_bloom', balance=10000.00, api_key=api_key)
     mike_bloom.set_password('password')
     mike_bloom.save()
+    # buy_trade = Trade(accounts_pk = mike_bloom.pk, ticker='tsla', volume=10, price=100.0)
+    # sell_trade = Trade(accounts_pk = mike_bloom.pk, ticker='tsla', volume=-5, price=200.0)
+    # buy_trade.save()
+    # sell_trade.save()
 
-    buy_trade = Trade(accounts_pk = mike_bloom.pk, ticker='tsla', volume=10, price=100.0)
-    sell_trade = Trade(accounts_pk = mike_bloom.pk, ticker='tsla', volume=-5, price=200.0)
-    buy_trade.save()
-    sell_trade.save()
-
-    tsla_position = Position(ticker='tsla', shares=5, accounts_pk=mike_bloom.pk)
-    tsla_position.save()
+    # tsla_position = Position(ticker='tsla', shares=5, accounts_pk=mike_bloom.pk)
+    # tsla_position.save()

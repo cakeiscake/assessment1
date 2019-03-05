0c81d8c14feb03619611eecd4f1dd6c375aeb35e
cake <liveinfantasy@gmail.com>
2019-03-05 12:15:11 -0500

    Working seedo

--- sqldbphase1/data/seed.py
+++ sqldbphase1/data/seed.py
@@ -9,10 +9,25 @@ DBPATH = os.path.join(DIR, DBNAME)
 
 def seed(dbpath=DBPATH):
     ORM.dbpath = dbpath
-    studentID = genID()
     new_york = Campus(state='New York', city='New York')
     houston = Campus(state='Texas', city='Houston')
-    Locket = Student(first_name='test', last_name='testname',studentID=studentID, gpa=3.0)
     new_york.save()
     houston.save()
-    # Locket.save()
+        
+    walker = Student(campus_pk=new_york.pk, first_name='Walker', last_name='Locket',studentID=genID(), gpa=3.1)
+    casey = Student(campus_pk=new_york.pk, first_name='Casey', last_name='Walker',studentID=genID(), gpa=2.7)
+    frank = Student(campus_pk=new_york.pk, first_name='Franklyn', last_name='Kilome',studentID=genID(), gpa=3.8)
+    hecton = Student(campus_pk=new_york.pk, first_name='Hecton', last_name='Santiago',studentID=genID(), gpa=2.9)
+    walker.save()
+    casey.save()
+    frank.save()
+    hecton.save()
+
+    framber = Student(campus_pk=houston.pk, first_name='Framber', last_name='Valdez', studentID=genID(), gpa=3.9)
+    brad = Student(campus_pk=houston.pk, first_name='Brad', last_name='Peacock', studentID=genID(), gpa=2.8)
+    reymin = Student(campus_pk=houston.pk, first_name='Guduan', last_name='Reymin', studentID=genID(), gpa=3.5)
+    gerrit = Student(campus_pk=houston.pk, first_name='Cole', last_name='Gerrit', studentID=genID(), gpa=3.0)
+    framber.save()
+    brad.save()
+    reymin.save()
+    gerrit.save()
